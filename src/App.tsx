a4b1c6f7f8d10c90708979481636ef6718575609
mann-jain143 <mannjain4885@gmail.com>
2026-06-26 21:23:11 +0530

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename="/modern-portfolio-website">
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
